5857d4abd4725cc7997613e8e0e0de99d0c925a5
Anton Dyulgerov <adyulgerov@tu-sofia.bg>
2024-12-17 15:16:04 +0200

    Add useForm and Cars-Hooks

---
+++ src/hooks/cars-hooks.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+import * as carApi from '../api/carApi';
+ 
+ 
+export function useGetAllCars() {
+ 
+    const [cars, setCars] = useState([]);
+ 
+    useEffect(() => {
+ 
+        carApi.getAllCars()
+            .then(result => {
+ 
+                setCars(result);
+            });
+    }, []);
+ 
+    return [cars];
+}
+ 
+ 
+export function useFilterCars(carName) {
+ 
+    const [cars, setCars] = useState([]);
+ 
+    useEffect(() => {
+ 
+        carApi.filterCars(carName)
+            .then(result => {
+ 
+ 
+                setCars(result);
+            });
+ 
+    }, [carName]);
+ 
+ 
+    return [cars];
+}
+ 
+ 
+export function useAddNewCar() {
+    return carApi.addNewCar;
+}
